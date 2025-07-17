f883edcb01d730c6fd08601274e954641beefcd5
gokxhan <hgkaradag@gmail.com>
2025-07-17 10:58:14 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,1 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="tr">
-      <body className="bg-slate-900 text-slate-100">{children}</body>
-    </html>
-  );
-}
+export default function RootLayout({ children }: { children: React.ReactNode }) { return <html lang="tr"><body className="bg-slate-900 text-slate-100">{children}</body></html>; }
